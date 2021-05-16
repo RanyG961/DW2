fb6fde1117703651d88599f9ba9bc0a9b13e9d98
ranyg961 <ranywwe@hotmail.com>
2021-05-16 21:40:23 +0200

    Ajout de retour a l espace utilisateur quand on est sur l'editeur de texte

---
+++ src/main/java/dw2/projetweb/servlets/AjouteAmi.java
@@ -0,0 +1,37 @@
+package dw2.projetweb.servlets;
+
+import dw2.projetweb.forms.FormFichier;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/AjoutAmi")
+public class AjouteAmi extends HttpServlet
+{
+    private static final FormFichier f = new FormFichier();
+    public static final String VUE = "/WEB-INF/Site/espaceUtilisateur.jsp";
+    public static final String SERVLET = "/EspaceUtilisateur";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+    {
+        this.getServletContext().getRequestDispatcher(SERVLET).forward(req, res);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+    {
+        try
+        {
+            f.ajoutAmis(req);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        this.getServletContext().getRequestDispatcher(SERVLET).forward(req, res);
+    }
+}
